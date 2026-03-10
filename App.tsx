cff6a4ee92868de0f16cf8d7dc57a39bab9b11eb
imrahul05 <rahulkumar20000516@gmail.com>
2026-03-10 21:10:09 +0530

    feat: Conditionally render ElevenLabsAgent component exclusively on the root path.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { AppRoutes } from './components/routes/Routes';
 import { ElevenLabsAgent } from './components/ElevenLabsAgent';
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
       <AppRoutes />
-      <ElevenLabsAgent />
+      {location.pathname === '/' && <ElevenLabsAgent />}
     </>
   );
 }
