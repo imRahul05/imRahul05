37f508657c0a8ee203e3ce776b8cfc9714557bf2
imrahul05 <rahulkumar20000516@gmail.com>
2026-05-07 12:41:51 +0530

    Remove TypeScript configuration and Vite setup files

--- components/SectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface SectionTitleProps {
-  children: React.ReactNode;
-}
-
-export const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => (
-  <h2 className="section-title">{children}</h2>
-);
--- pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { HomePage } from './HomePage';
-export { ProjectsPage } from './ProjectsPage';
-export { BlogsPage } from './BlogsPage';
-export { default as BlogDetailPage } from './BlogDetailPage';
